ccd6de1bdf426e913301a9042ad9035982e0bd14
Hitesh Malhotra <47815600+HiteshM2107@users.noreply.github.com>
2020-09-01 14:41:54 +0530

    Updates

---
+++ precache-manifest.88db851f76d134eba6beb5ba00377b5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "059c3bc49e0c3a5500643f4acae77432",
+    "url": "/Weather-fy/index.html"
+  },
+  {
+    "revision": "896e5c7779340653b77e",
+    "url": "/Weather-fy/static/css/main.e323f44e.chunk.css"
+  },
+  {
+    "revision": "7cf2182656a62ed6117a",
+    "url": "/Weather-fy/static/js/2.d437b892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-fy/static/js/2.d437b892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "896e5c7779340653b77e",
+    "url": "/Weather-fy/static/js/main.689d0863.chunk.js"
+  },
+  {
+    "revision": "7a95b37d6f799c9340c5",
+    "url": "/Weather-fy/static/js/runtime-main.ee51f7a4.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/Weather-fy/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/Weather-fy/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
